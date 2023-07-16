d705a2ed03ed15ee9c883833625e00af1e5859b6
ali reza <a.rezamohmmadi369@gmail.com>
2023-07-16 17:44:07 +0330

    25/4

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
+
 $(document).ready(function () {
+  // CHECK TOKEN
+  if (!localStorage.getItem("token")) {
+    console.log("run away");
+    window.location = "/login.html";
+  }
+
   // RESPONSIVE
   $(".habergure").click(function () {
     $(".right-side").toggleClass("is-active");
   });
 
+  // USER AUTH
+  $(".log-out").click(function () {
+    localStorage.removeItem("token");
+    window.location = "/login.html";
+  });
 });
